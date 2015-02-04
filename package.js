f6ee622ed4baaf0bd6f1ea67bc5a6326c04a7b6b
Richard Lai <it.rclai@gmail.com>
2015-02-04 14:24:54 +0000

    Bumped version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lai:ddp-inspector',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Allows you to see all DDP activity in the client-side.',
   // URL to the Git repository containing the source code for this package.
